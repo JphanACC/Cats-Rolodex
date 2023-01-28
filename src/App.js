c2758fa8af32e9d081ff6bcab18a0d7a67f90fcb
JPhan <jphanacc@gmail.com>
2023-01-27 17:44:48 -0700

    Working Searching Filter text box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+import {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  constructor() {
+    //inherit everything that it extends from
+    super();
+    console.log('1. constructor');
+    //state is alwas a JSON object
+    this.state = {
+      cats: [],
+      searchField: '',
+    }
+  }
+
+  componentDidMount() {
+    console.log('3. componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      //then() makes it become a Promise
+      //response is the callback 
+      .then(
+        response => response.json()
+      )
+      .then(
+        (users) => this.setState(() => {
+          return {cats: users}
+        },
+        () => {
+          console.log('success response')
+          console.log(this.state.cats)
+        }
+        )
+      )
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(()=> {
+      return { searchField }
+    });
+  }
+
+  render() {
+    console.log('2. render');
+    const { cats, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    //This depends on the state
+    const filteredCats = this.state.cats.filter( (cat) => {return cat.name.toLowerCase().includes(this.state.searchField);});
+
+    return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search cats' 
+          onChange={this.onSearchChange}/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>List of Cat Names</h1>
+        {filteredCats.map( (cat) => {
+          return (
+            <div key={cat.id}> 
+              <h1>{cat.name}</h1>
+            </div>
+          )
+        })}
+        
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
